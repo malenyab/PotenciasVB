0cb65745dfc784b33240070ca49267dcc1fb2a7f
malenyab <maleny_1292@hotmail.com>
2017-03-11 18:31:49 -0600

    Add files via upload

---
+++ potenciasVB/potenciasVB/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim valor As Double
+        Dim potencia As Double
+        Dim resultado As Double
+
+        valor = Convert.ToDouble(TextBox1.Text)
+        potencia = Convert.ToDouble(TextBox2.Text)
+
+        resultado = valor ^ potencia
+
+        Label3.Text = resultado.ToString
+
+    End Sub
+End Class
